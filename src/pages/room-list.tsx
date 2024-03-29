3dd3bdab0cc5d31cd674fd6083c0a97b6cfe34a0
ehdrjs4502 <ehdrjs4502@naver.com>
2024-03-29 19:15:20 +0900

    테스트 데이터 분리

--- src/pages/room-list.tsx
+++ src/pages/room-list.tsx
@@ -2,147 +2,17 @@ import ListCard from "../components/ui/list-card";
 import { Room } from "../types";
 import styles from "../styles/room-list.module.css";
 import Navigation from "../components/navigation";
+import { roomListData } from "../data/room-list-data";
 
 export default function RoomList() {
-  const roomList: Room[] = [
-    {
-      id: 1,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "1/2",
-      isWait: true,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-    {
-      id: 2,
-      title: "1ㄷ1 뜨실 초보만",
-      members: "3/3",
-      isWait: false,
-      password: null,
-      settings: {
-        difficulty: "브론즈1",
-        timeLimit: "1h",
-        numOfSubmissions: 5,
-        lang: "JAVA",
-      },
-    },
-  ];
+  const roomList = roomListData; // 테스트 데이터
   return (
     <div>
       <Navigation />
       <div className={styles[`list-container`]}>
         <div className={styles.top}>
           <div>
-            <input />
+            <input placeholder="방 번호로 입장하기" type="number" />
             <button>입장</button>
           </div>
           <div>
