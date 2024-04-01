ee2803d4b936dbcc2d7828b7ceff1fd255430fb9
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-01 17:02:07 +0900

    유저 정보 클릭 이벤트 추가

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,11 +2,20 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { IconButton } from "@mui/material";
 import styles from "../styles/navigation.module.css";
+import { addTab } from "../utils/tabs";
+import User from "./tabs/user";
 
-export default function Navigation() {
+interface NavigationProps {
+  dockLayoutRef: React.RefObject<any>; // DockLayout 컴포넌트에 대한 RefObject 타입 지정
+}
+
+export default function Navigation({ dockLayoutRef }: NavigationProps) {
+  const onClickUserButton = () => {
+    addTab("유저 정보", <User />, dockLayoutRef);
+  };
   return (
     <nav className={styles.navigation}>
-      <IconButton size="large">
+      <IconButton size="large" onClick={onClickUserButton}>
         <AccountCircleIcon />
       </IconButton>
       <IconButton size="large">
