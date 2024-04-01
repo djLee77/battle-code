d738df7782a22f050bea233eef2939db290011c5
djLee77 <dkdnl232@gmail.com>
2024-04-01 17:11:57 +0900

    로그인 유지 구현
    : refreshToken으로 acceessToken 발급을 요청하고 발급이 안되면 로그인 페이지로 이동

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,8 +1,12 @@
 import DockLayout from "rc-dock";
 import RoomList from "../components/tabs/room-list";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import "../styles/rc-dock-dark.css"; // 다크모드(커스텀)
 import Navigation from "../components/navigation";
+import { getRefreshToken, removeRefreshToken } from "../utils/cookie";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import useAuthStore from "../store/userStore";
 
 // 탭 형식에 맞게 만드는 함수
 function getTab(id: string, component: any) {
@@ -14,6 +18,28 @@ function getTab(id: string, component: any) {
 }
 
 export default function MainPage() {
+  const navigate = useNavigate();
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
+  const { setAccessToken } = useAuthStore();
+
+  useEffect(() => {
+    const refreshToken = getRefreshToken();
+    if (refreshToken) {
+      axios
+        .post(`${serverUrl}/api/refresh`, { refreshToken })
+        .then((response) => {
+          const { accessToken } = response.data;
+          setAccessToken(accessToken); // Zustand 스토어에 accessToken 저장
+        })
+        .catch(() => {
+          removeRefreshToken(); // 쿠키에서 refreshToken 제거
+          navigate("/login"); // 로그인 페이지로 리다이렉션
+        });
+    } else {
+      alert("토큰이 만료되어 로그인 페이지로 이동합니다.");
+      navigate("/login"); // refreshToken이 없으면 로그인 페이지로 리다이렉션
+    }
+  }, [navigate, setAccessToken, serverUrl]);
   const dockLayoutRef = useRef(null); // DockLayout 컴포넌트에 대한 ref 생성
   // 초기 레이아웃 설정
   const defaultLayout: any = {
