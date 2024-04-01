241fc22fb1c3c2c557598111c79f3a58a1a12774
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-01 19:08:57 +0900

    유저 탭 구현

--- src/components/tabs/user.tsx
+++ src/components/tabs/user.tsx
@@ -1,9 +1,29 @@
+import CircleProgress from "components/circle";
+import RecordCard from "components/record-card";
+import CustomBtn from "components/ui/button";
+import { userData } from "data/user-data";
 import styles from "styles/user.module.css";
 
 export default function User() {
+  const userTestData = userData;
   return (
-    <div>
-      <h3>유저 정보</h3>
+    <div className={styles[`user-container`]}>
+      <div className={styles[`user-info`]}>
+        <h2>{userTestData.name}</h2>
+        <span>랭킹:{userTestData.rank}위</span>
+      </div>
+      <div>
+        <span>사용 테마 : {userTestData.useTheme}</span>
+        <CustomBtn size="small">테마 변경</CustomBtn>
+      </div>
+      <div className={styles[`record-container`]}>
+        <CircleProgress progress={60} size={240} strokeWidth={30} circleColor="#FF5F58" progressColor="#3278FF" />
+        <div className={styles[`record-box`]}>
+          {userTestData.battleRecordList.map((record, idx) => (
+            <RecordCard key={idx} record={record} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
