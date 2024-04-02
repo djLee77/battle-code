15d80c8d8d87c18cfd86d216929fd490e2845fa1
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-02 15:53:01 +0900

    함수 수정

--- src/utils/tabs.ts
+++ src/utils/tabs.ts
@@ -11,8 +11,7 @@ export const getTab = (id: string, component: any, isClose: boolean) => {
 // 탭 추가 함수
 export const addTab = (tabName: string, component: any, dockLayoutRef: React.RefObject<any>) => {
   // 이미 탭에 존재하는지 확인
-  const isExist = dockLayoutRef.current.find("my_panel").tabs.some((dock: any) => dock.id === tabName);
-  if (!isExist) {
+  if (!dockLayoutRef.current.updateTab(tabName, getTab(tabName, component, true))) {
     const newTab = getTab(tabName, component, true); // 새로운 탭 생성
     dockLayoutRef.current.dockMove(newTab, "my_panel", "middle"); // 생성한 탭을 DockLayout에 추가
   }
