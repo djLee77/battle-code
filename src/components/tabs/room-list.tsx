7249715b79c40b9b523eebeed33bd7accb32f2a9
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-02 15:43:38 +0900

    height 동적으로 변경

--- src/components/tabs/room-list.tsx
+++ src/components/tabs/room-list.tsx
@@ -5,9 +5,26 @@ import { roomListData } from "data/room-list-data";
 import RoomEntry from "components/room-entry";
 import CustomBtn from "components/ui/button";
 import CreateRoomModal from "components/modals/create-room";
+import { useEffect, useState } from "react";
 
 export default function RoomList() {
   const roomList = roomListData; // 테스트 데이터
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  console.log(viewportHeight);
+
   return (
     <div className={styles[`list-container`]}>
       <div className={styles.top}>
@@ -19,7 +36,7 @@ export default function RoomList() {
           <CreateRoomModal />
         </div>
       </div>
-      <div className={styles.list}>
+      <div className={styles.list} style={{ height: viewportHeight * 0.7 }}>
         {roomList.map((room: Room) => (
           <ListCard key={room.id} room={room} />
         ))}
