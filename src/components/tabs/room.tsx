99c20ba08312a8295e64b633f1c5dc15171cc9c5
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-11 17:42:32 +0900

    룸 컴포넌트 생성

---
+++ src/components/tabs/room.tsx
@@ -0,0 +1,12 @@
+export default function Room() {
+  return (
+    <div>
+      <div>코딩테스트문제</div>
+      <div>
+        <div>방 유저 목록</div>
+        <div>방 설정값</div>
+      </div>
+      <div>채팅창</div>
+    </div>
+  );
+}
