6e2f928f077532309b316a218087c65f353203fc
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-12 19:56:32 +0900

    대기방 레이아웃 설정

--- src/components/tabs/room.tsx
+++ src/components/tabs/room.tsx
@@ -2,13 +2,25 @@ import styles from "styles/room.module.css";
 
 export default function Room() {
   return (
-    <div className={styles.container}>
-      <div>코딩테스트문제</div>
-      <div>
-        <div>방 유저 목록</div>
-        <div>방 설정값</div>
+    <div>
+      <h2 className={styles.title}>방 제목</h2>
+      <div className={styles.container}>
+        <div className={styles[`test-problem`]}>코딩테스트문제</div>
+        <div className={styles["room-info"]}>
+          <div className={styles["user-list"]}>방 유저 목록</div>
+          <div className={styles["room-settings"]}>방 설정값</div>
+        </div>
+        <div className={styles[`chat`]}>채팅창</div>
+        <div className={styles[`button-wrapper`]}>
+          <button className={styles.button}>버튼 1</button>
+        </div>
+        <div className={styles[`button-wrapper`]}>
+          <button className={styles.button}>버튼 2</button>
+        </div>
+        <div className={styles[`button-wrapper`]}>
+          <button className={styles.button}>채팅 숨기기</button>
+        </div>
       </div>
-      <div>채팅창</div>
     </div>
   );
 }
