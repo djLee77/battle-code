d851d129df15872a9d9fe125b6a369beaf1d4ecf
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-14 16:13:04 +0900

    채팅 숨기기 구현

--- src/components/tabs/room.tsx
+++ src/components/tabs/room.tsx
@@ -7,24 +7,23 @@ export default function Room() {
   return (
     <div>
       <h2 className={styles.title}>방 제목</h2>
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        style={chatIsHide ? { gridTemplateColumns: "40% 60%" } : { gridTemplateColumns: "34% 42% 23%" }}
+      >
         <div className={styles[`test-problem`]}>코딩테스트문제</div>
         <div className={styles["room-info"]}>
           <div className={styles["user-list"]}>방 유저 목록</div>
           <div className={styles["room-settings"]}>방 설정값</div>
         </div>
         {!chatIsHide && <div className={styles[`chat`]}>채팅창</div>}
-        <div className={styles[`button-wrapper`]}>
-          <button className={styles.button}>버튼 1</button>
-        </div>
-        <div className={styles[`button-wrapper`]}>
-          <button className={styles.button}>버튼 2</button>
-        </div>
-        <div className={styles[`button-wrapper`]}>
-          <button className={styles.button} onClick={() => setChatIsHide(!chatIsHide)}>
-            채팅 숨기기
-          </button>
-        </div>
+      </div>
+      <div className={styles[`button-wrapper`]}>
+        <button className={styles.button}>버튼 1</button>
+        <button className={styles.button}>버튼 2</button>
+        <button className={styles.button} onClick={() => setChatIsHide(!chatIsHide)}>
+          채팅 숨기기
+        </button>
       </div>
     </div>
   );
