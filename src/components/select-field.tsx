b6ce1ee5d2bb2521f83b931b674b06bacb99b10d
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-16 00:04:05 +0900

    공통 컴포넌트 생성

---
+++ src/components/select-field.tsx
@@ -0,0 +1,33 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+import styles from "styles/select-field.module.css";
+
+// select option 타입
+type Option = {
+  value: string;
+  name: string;
+};
+
+export default function SelectField({
+  label,
+  register,
+  options,
+}: {
+  label: string;
+  register: UseFormRegisterReturn;
+  options: Option[];
+}) {
+  return (
+    <div className={styles["select-box"]}>
+      <div className={styles.select}>
+        <label htmlFor={register.name}>{label} </label>
+        <select id={register.name} {...register}>
+          {options.map((item: Option) => (
+            <option value={item.value} key={item.value}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
