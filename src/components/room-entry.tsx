c2b5f31631b490fb41199543dd1a92d7d8b6fe53
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-16 01:33:42 +0900

    stomp 메시지 전송 테스트

--- src/components/room-entry.tsx
+++ src/components/room-entry.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import CustomButton from "./ui/button";
 import styles from "styles/room-entry.module.css";
+import useWebSocketStore from "store/websocketStore";
 
 export default function RoomEntry() {
   const [value, setValue] = useState("");
+  const { publishMessage } = useWebSocketStore();
+  // 메시지 발행 예시
+  const handleSendMessage = () => {
+    const destination = "/app/chat"; // 메시지를 발행할 대상 destination 설정
+    const messageBody = "Hello from WebSocketTestComponent!"; // 발행할 메시지 내용
+    publishMessage(destination, messageBody);
+  };
 
   const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     // 숫자만 저장
@@ -13,7 +21,7 @@ export default function RoomEntry() {
   return (
     <div className={styles[`room-entry-container`]}>
       <input placeholder="방 번호로 입장하기" value={value} onChange={(e) => handleInputValue(e)} />
-      <CustomButton type="button" size="small" onClick={() => {}}>
+      <CustomButton type="button" size="small" onClick={handleSendMessage}>
         방 입장
       </CustomButton>
     </div>
