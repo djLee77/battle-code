cc7ebee14a41189ac3a941000d6759368ae6a538
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-16 19:37:33 +0900

    소켓 연결시 default room 구독

--- src/store/websocketStore.ts
+++ src/store/websocketStore.ts
@@ -21,10 +21,19 @@ const useWebSocketStore = create<WebSocketStoreState>((set) => ({
       connectHeaders: {
         Authorization: `Bearer ${refreshToken}`,
       },
+      reconnectDelay: 0, // 0은 자동 재연결 비활성화
       onConnect: () => {
         console.log("연결 성공");
         // 연결 됐으면 연결 상태 true로 변경
         set((state) => ({ ...state, isConnected: true }));
+        // 연결 성공 후 default room 구독
+        client.subscribe("/topic/public/room", (message) => {
+          console.log("받은 메시지:", message.body);
+          // 원하는 작업을 수행
+        });
+      },
+      onDisconnect: () => {
+        console.log("연걸 해제");
       },
     });
 
