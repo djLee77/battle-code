e407cc37893e81a47ce3b16dd9830689bbbcc3b7
djLee77 <dkdnl232@gmail.com>
2024-04-16 17:32:41 +0900

    feat : 로그아웃 기능 구현

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,21 +4,31 @@ import { IconButton } from "@mui/material";
 import styles from "../styles/navigation.module.css";
 import { addTab } from "../utils/tabs";
 import User from "./tabs/user";
+import { removeRefreshToken, removeAccessToken } from "../utils/cookie";
+import { useNavigate } from "react-router-dom";
 
 interface NavigationProps {
   dockLayoutRef: React.RefObject<any>; // DockLayout 컴포넌트에 대한 RefObject 타입 지정
 }
 
 export default function Navigation({ dockLayoutRef }: NavigationProps) {
+  const navigate = useNavigate();
+  
   const onClickUserButton = () => {
     addTab("유저 정보", <User />, dockLayoutRef);
   };
+
+  const onClickLogout = () => {
+    removeRefreshToken();
+    removeAccessToken();
+    navigate('/login');
+  }
   return (
     <nav className={styles.navigation}>
       <IconButton size="large" onClick={onClickUserButton}>
         <AccountCircleIcon />
       </IconButton>
-      <IconButton size="large">
+      <IconButton size="large" onClick={onClickLogout}>
         <LogoutIcon />
       </IconButton>
     </nav>
