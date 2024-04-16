fbd5a538e81e0d0c7b630bc1f42d160f1d9e2b59
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-17 04:15:29 +0900

    value 변경

--- src/data/room-setting-data.ts
+++ src/data/room-setting-data.ts
@@ -1,8 +1,8 @@
 export const levelData = [
-  { value: "default", name: "상관없음" },
-  { value: "b1", name: "브론즈1" },
-  { value: "s1", name: "실버1" },
-  { value: "g1", name: "골드1" },
+  { value: 1, name: "상관없음" },
+  { value: 2, name: "브론즈1" },
+  { value: 3, name: "실버1" },
+  { value: 4, name: "골드1" },
 ];
 
 export const langData = [
@@ -13,8 +13,8 @@ export const langData = [
 ];
 
 export const limitTImeData = [
-  { value: "default", name: "15분" },
-  { value: "30m", name: "30분" },
-  { value: "1h", name: "1시간" },
-  { value: "2h", name: "2시간" },
+  { value: 15, name: "15분" },
+  { value: 30, name: "30분" },
+  { value: 60, name: "1시간" },
+  { value: 120, name: "2시간" },
 ];
