9f3ad3c0022ac9d1bbe59ef86c297e5b25f06946
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-19 01:11:23 +0900

    탭 제거 함수 추가

--- src/utils/tabs.ts
+++ src/utils/tabs.ts
@@ -8,17 +8,27 @@ export const getTab = (id: string, component: any, isClose: boolean) => {
   };
 };
 
+// 탭 제거 함수
+export const removeTab = (tabsArray: any, idSubstring: string, dockLayoutRef: any) => {
+  tabsArray.forEach((child: any) => {
+    const tabName = child.tabs?.find((tab: any) => tab.id.includes(idSubstring));
+    if (tabName) {
+      dockLayoutRef.current.dockMove(tabName, null, "remove");
+    }
+  });
+};
+
 // 탭 추가 함수
 export const addTab = (tabName: string, component: any, dockLayoutRef: React.RefObject<any>) => {
   // 이미 탭에 존재하는지 확인
   if (!dockLayoutRef.current.updateTab(tabName, getTab(tabName, component, true))) {
     // 대기방 추가할 때 대기방 탭이 존재하는지 확인 존재할시 삭제
     if (tabName.includes("번방")) {
-      dockLayoutRef.current.state.layout.dockbox.children.map((child: any) => {
-        const tabName = child.tabs?.filter((tab: any) => tab.id.includes("번방"))[0];
-        console.log(tabName);
-        dockLayoutRef.current.dockMove(tabName, null, "remove");
-      });
+      // dockbox 안의 children 탭 제거
+      removeTab(dockLayoutRef.current.state.layout.dockbox.children, "번방", dockLayoutRef);
+
+      // floatbox 안의 children 탭 제거
+      removeTab(dockLayoutRef.current.state.layout.floatbox.children, "번방", dockLayoutRef);
     }
 
     const newTab = getTab(tabName, component, true); // 새로운 탭 생성
