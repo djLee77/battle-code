20f4d89068eb4477c99ecfee540790d8b8d79458
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-22 22:27:08 +0900

    레디 구현

--- src/components/tabs/room.tsx
+++ src/components/tabs/room.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { UserCard } from "components/user-card";
+import ModifyRoomModal from "components/modals/modify-room";
+import UserCard from "components/user-card";
 import { useEffect, useState } from "react";
 import useWebSocketStore from "store/websocket-store";
 import styles from "styles/room.module.css";
@@ -16,7 +17,7 @@ export default function Room({ data, dockLayoutRef }: IProps) {
   const [chatIsHide, setChatIsHide] = useState<boolean>(false);
   const [roomStatus, setRoomStatus] = useState(data.roomStatus);
   const [userStatus, setUserStatus] = useState(data.userStatus);
-  const { webSocketClient } = useWebSocketStore();
+  const { webSocketClient, publishMessage } = useWebSocketStore();
   const serverUrl = process.env.REACT_APP_SERVER_URL;
 
   const handleRoomLeave = async () => {
@@ -37,11 +38,45 @@ export default function Room({ data, dockLayoutRef }: IProps) {
     }
   };
 
+  // 준비 버튼 누르면 누른 유저의 정보 소켓으로 전송
+  const handleReady = () => {
+    const updateUser = userStatus.filter((user) => user.userId === "test")[0];
+    updateUser.isReady = !updateUser.isReady;
+    console.log(updateUser);
+    publishMessage(`/app/room/${data.roomStatus.roomId}/update/user-status`, updateUser);
+  };
+
+  // 유저 상태 업데이트하는 함수
+  const handleUpdateUserStatus = (updateUser: any) => {
+    const updatedUserStatusArray = userStatus.reduce((acc: any, user: any) => {
+      if (user.userId === updateUser.userId) {
+        // userId가 같은 경우 원하는 객체로 교체하여 새로운 배열 생성
+        acc.push(updateUser);
+      } else {
+        // 그 외의 경우는 기존 객체 그대로 유지하여 새로운 배열 생성
+        acc.push(user);
+      }
+      return acc;
+    }, []);
+    console.log("업데이트 : ", updatedUserStatusArray);
+    setUserStatus(updatedUserStatusArray);
+  };
+
   // 첫 마운트 될 때 방 구독하기, 언마운트 될 때 구독 취소하기
   useEffect(() => {
     if (webSocketClient) {
-      const subscription = webSocketClient.subscribe("/topic/room/1", (message) => {
-        console.log("Received message:", message.body);
+      const subscription = webSocketClient.subscribe(`/topic/room/${data.roomStatus.roomId}`, (message) => {
+        const receivedMessage = JSON.parse(message.body);
+        console.log("Received message:", receivedMessage);
+        // 받은 메시지가 유저 상태 객체면 handleUpdateUserStatus 함수 실행
+        if (receivedMessage.userStatus) {
+          return handleUpdateUserStatus(receivedMessage.userStatus);
+        }
+
+        // 받은 메시지가 방 상태 객체면 방 상태 업데이트
+        if (receivedMessage.roomStatus) {
+          setRoomStatus(receivedMessage.roomStatus);
+        }
       });
 
       console.log(data);
@@ -69,7 +104,13 @@ export default function Room({ data, dockLayoutRef }: IProps) {
 
   return (
     <div>
-      <h2 className={styles.title}>{roomStatus.title}</h2>
+      <div className={styles[`title-box`]}>
+        <h2 className={styles.title}>{roomStatus.title}</h2>
+        {
+          // user-store 완성되면 수정해야함
+          roomStatus.hostId === "test" && <ModifyRoomModal data={data.roomStatus} />
+        }
+      </div>
       <div style={!chatIsHide ? { display: "none" } : { display: "block", position: "absolute", right: 10, top: 10 }}>
         <button className={styles.button} onClick={() => setChatIsHide(!chatIsHide)}>
           채팅 On
@@ -102,7 +143,7 @@ export default function Room({ data, dockLayoutRef }: IProps) {
         <button className={styles.button} onClick={handleTabRemove}>
           나가기
         </button>
-        <button className={styles.button} style={{ marginLeft: "47%" }}>
+        <button className={styles.button} style={{ marginLeft: "47%" }} onClick={handleReady}>
           준비 완료
         </button>
       </div>
