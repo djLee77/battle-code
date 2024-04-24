52b3394d3f53cfb3dd5c9ac84827b1af27ae4a5c
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-24 17:11:20 +0900

    탭 함수 수정

--- src/utils/tabs.ts
+++ src/utils/tabs.ts
@@ -4,7 +4,7 @@ export const getTab = (id: string, component: any, isClose: boolean) => {
     id, // 탭의 고유한 ID
     content: component, // 탭 내용
     title: id, // 탭 제목
-    closable: isClose,
+    closable: id.includes("번방") ? false : isClose,
   };
 };
 
@@ -12,7 +12,9 @@ export const getTab = (id: string, component: any, isClose: boolean) => {
 export const removeTab = (tabsArray: any, idSubstring: string, dockLayoutRef: any) => {
   tabsArray.forEach((child: any) => {
     const tabName = child.tabs?.find((tab: any) => tab.id.includes(idSubstring));
+    console.log("탭 이름 :", tabName);
     if (tabName) {
+      console.log(dockLayoutRef);
       dockLayoutRef.current.dockMove(tabName, null, "remove");
       console.log("제거", dockLayoutRef);
     }
@@ -21,18 +23,21 @@ export const removeTab = (tabsArray: any, idSubstring: string, dockLayoutRef: an
 
 // 탭 추가 함수
 export const addTab = (tabName: string, component: any, dockLayoutRef: React.RefObject<any>) => {
-  // 이미 탭에 존재하는지 확인
-  if (!dockLayoutRef.current.updateTab(tabName, getTab(tabName, component, true))) {
-    // 대기방 추가할 때 대기방 탭이 존재하는지 확인 존재할시 삭제
-    if (tabName.includes("번방")) {
-      // dockbox 안의 children 탭 제거
-      removeTab(dockLayoutRef.current.state.layout.dockbox.children, "번방", dockLayoutRef);
-
-      // floatbox 안의 children 탭 제거
-      removeTab(dockLayoutRef.current.state.layout.floatbox.children, "번방", dockLayoutRef);
-    }
-
+  // 대기방 추가할 때 대기방 탭이 존재하는지 확인 존재할시 삭제
+  if (tabName.includes("번방")) {
+    // dockbox 안의 children 탭 제거
+    removeTab(dockLayoutRef.current.state.layout.dockbox.children, "번방", dockLayoutRef);
+    // floatbox 안의 children 탭 제거
+    removeTab(dockLayoutRef.current.state.layout.floatbox.children, "번방", dockLayoutRef);
     const newTab = getTab(tabName, component, true); // 새로운 탭 생성
     dockLayoutRef.current.dockMove(newTab, "my_panel", "middle");
+    return;
+  }
+
+  // 이미 탭에 존재하는지 확인
+  if (!dockLayoutRef.current.updateTab(tabName, getTab(tabName, component, true))) {
+    const newTab = getTab(tabName, component, true); // 새로운 탭 생성
+    dockLayoutRef.current.dockMove(newTab, "my_panel", "middle");
+    return;
   }
 };
