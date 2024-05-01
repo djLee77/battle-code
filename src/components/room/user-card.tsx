7ad432130c167fcc2bed985dd82c049f4d05958b
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-01 23:33:27 +0900

    feat : 유저 언어 변경

--- src/components/room/user-card.tsx
+++ src/components/room/user-card.tsx
@@ -1,22 +1,42 @@
 import styles from "styles/user-card.module.css";
 import { UserCardInfo } from "./user-card-info";
 import React from "react";
+import { IUserStatus } from "types/room-types";
 
-const UserCard = ({ data }: any) => (
-  <div className={styles.container}>
-    <div className={styles[`dot-box`]}>
-      <span
-        className={styles.dot}
-        style={data.isReady ? { backgroundColor: "green" } : { backgroundColor: "yellow" }}
-      />
-      <span className={styles.dot} />
-    </div>
-    <div className={styles[`user-info`]}>
-      <UserCardInfo type="const" name="name" data={data.userId} />
-      <UserCardInfo type="let" name="lang" data={data.language} />
-      <UserCardInfo type="let" name="isReady" data={data.isReady ? "true" : "false"} />
-    </div>
-  </div>
-);
+interface IProps {
+  data: IUserStatus;
+  handleLanguageChange: any;
+}
 
+const UserCard = ({ data, handleLanguageChange }: IProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles[`dot-box`]}>
+        <span
+          className={styles.dot}
+          style={data.isReady ? { backgroundColor: "green" } : { backgroundColor: "yellow" }}
+        />
+        <span className={styles.dot} />
+      </div>
+      <div className={styles[`user-info`]}>
+        <UserCardInfo type="const" name="name" data={data.userId} />
+        <div>
+          <span className={styles["var-type-color"]}>const</span> <span className={styles["var-name-color"]}>lang</span>{" "}
+          ={" "}
+          {data.userId === localStorage.getItem("id") ? (
+            <select value={data.language} onChange={(event) => handleLanguageChange(data.userId, event.target.value)}>
+              <option value="java">java</option>
+              <option value="python">python</option>
+              <option value="javascript">javascript</option>
+            </select>
+          ) : (
+            <span className={styles["var-data-color"]}>'{data.language}'</span>
+          )}
+          ;
+        </div>
+        <UserCardInfo type="let" name="isReady" data={data.isReady ? "true" : "false"} />
+      </div>
+    </div>
+  );
+};
 export default React.memo(UserCard);
