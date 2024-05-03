4398c426d8395def289613473eeecbe136a4ce70
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-03 16:59:49 +0900

    feat : 언어 선택 구현

--- src/components/room/user-card.tsx
+++ src/components/room/user-card.tsx
@@ -1,5 +1,4 @@
 import styles from "styles/user-card.module.css";
-import { UserCardInfo } from "./user-card-info";
 import React from "react";
 import { IUserStatus } from "types/room-types";
 
@@ -19,12 +18,19 @@ const UserCard = ({ data, handleLanguageChange }: IProps) => {
         <span className={styles.dot} />
       </div>
       <div className={styles[`user-info`]}>
-        <UserCardInfo type="const" name="name" data={data.userId} />
+        <div>
+          <span className={styles["var-type-color"]}>const</span> <span className={styles["var-name-color"]}>name</span>{" "}
+          = <span className={styles["var-data-color"]}>'{data.userId}'</span>;
+        </div>
         <div>
           <span className={styles["var-type-color"]}>const</span> <span className={styles["var-name-color"]}>lang</span>{" "}
           ={" "}
           {data.userId === localStorage.getItem("id") ? (
-            <select value={data.language} onChange={(event) => handleLanguageChange(data.userId, event.target.value)}>
+            <select
+              className={styles.select}
+              value={data.language}
+              onChange={(event) => handleLanguageChange(data.userId, event.target.value)}
+            >
               <option value="java">java</option>
               <option value="python">python</option>
               <option value="javascript">javascript</option>
@@ -34,7 +40,11 @@ const UserCard = ({ data, handleLanguageChange }: IProps) => {
           )}
           ;
         </div>
-        <UserCardInfo type="let" name="isReady" data={data.isReady ? "true" : "false"} />
+        <div>
+          <span className={styles["var-type-color"]}>let</span>{" "}
+          <span className={styles["var-name-color"]}>isReady</span> ={" "}
+          <span className={styles["var-data-color"]}>'{data.isReady ? "true" : "false"}'</span>;
+        </div>
       </div>
     </div>
   );
