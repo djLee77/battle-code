79c9152f4e77b02063c2329c71702d8c994509b1
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-03 18:09:16 +0900

    feat : 로그아웃시 토큰 삭제, 소켓연결 해제

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,6 +5,8 @@ import styles from "../styles/navigation.module.css";
 import { addTab } from "../utils/tabs";
 import User from "./tabs/user";
 import useWebSocketStore from "store/websocket-store";
+import { useNavigate } from "react-router-dom";
+import { removeAccessToken, removeRefreshToken } from "utils/cookie";
 
 interface NavigationProps {
   dockLayoutRef: React.RefObject<any>; // DockLayout 컴포넌트에 대한 RefObject 타입 지정
@@ -12,6 +14,7 @@ interface NavigationProps {
 
 export default function Navigation({ dockLayoutRef }: NavigationProps) {
   const { webSocketClient } = useWebSocketStore();
+  const navigate = useNavigate();
 
   const handleUserInfo = () => {
     addTab("유저 정보", <User />, dockLayoutRef);
@@ -19,6 +22,9 @@ export default function Navigation({ dockLayoutRef }: NavigationProps) {
 
   const handleLogout = () => {
     webSocketClient?.deactivate();
+    removeAccessToken();
+    removeRefreshToken();
+    navigate("/login");
     console.log(webSocketClient);
   };
   return (
@@ -26,8 +32,8 @@ export default function Navigation({ dockLayoutRef }: NavigationProps) {
       <IconButton size="large" onClick={handleUserInfo}>
         <AccountCircleIcon />
       </IconButton>
-      <IconButton size="large">
-        <LogoutIcon onClick={handleLogout} />
+      <IconButton size="large" onClick={handleLogout}>
+        <LogoutIcon />
       </IconButton>
     </nav>
   );
