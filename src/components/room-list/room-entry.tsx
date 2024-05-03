5b41e19fd5b4170c7bb7710c867ef1c2334792ba
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-03 18:10:41 +0900

    test : 테스트 함수 삭제

--- src/components/room-list/room-entry.tsx
+++ src/components/room-list/room-entry.tsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import CustomButton from "../ui/button";
 import styles from "styles/room-entry.module.css";
-import useWebSocketStore from "store/websocket-store";
 
 export default React.memo(function RoomEntry() {
   const [value, setValue] = useState("");
-  const { isConnected, publishMessage, subscribe } = useWebSocketStore();
-
-  // 메시지 발행 예시
-  const handleSendMessage = () => {
-    const destination = "/app/default/room"; // 메시지를 발행할 대상 destination 설정
-    const messageBody = { roomId: null, senderId: "test", message: "야이 짱구야" }; // 발행할 메시지 내용
-    publishMessage(destination, messageBody);
-  };
 
   const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     // 숫자만 저장
@@ -22,7 +13,7 @@ export default React.memo(function RoomEntry() {
   return (
     <div className={styles[`room-entry-container`]}>
       <input placeholder="방 번호로 입장하기" value={value} onChange={(e) => handleInputValue(e)} />
-      <CustomButton type="button" size="small" onClick={handleSendMessage}>
+      <CustomButton type="button" size="small">
         방 입장
       </CustomButton>
     </div>
