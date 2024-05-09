5ab85b6863afc059b8239afc357059f872144316
djLee77 <dkdnl232@gmail.com>
2024-05-09 14:57:18 +0900

    feat(code-editor) : 언어 변경 기능 구현

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,18 +1,20 @@
-import React from "react";
-import Editor, { EditorProps } from "@monaco-editor/react";
-import { useState } from "react";
+import React from 'react';
+import Editor, { EditorProps } from '@monaco-editor/react';
+import { useState } from 'react';
+import styles from 'styles/code-editor.module.css';
 
 const CodeEditor: React.FC = (): JSX.Element => {
   const [code, setCode] = useState<string>(
     "var message = 'Monaco Editor!' \nconsole.log(message);"
   );
+  const [language, setLanguage] = useState<string>('javascript');
 
-  const editorOptions: EditorProps["options"] = {
+  const editorOptions: EditorProps['options'] = {
     inlineSuggest: { enabled: true },
     fontSize: 16,
     formatOnType: true,
-    autoClosingBrackets: "always",
-    minimap: { scale: 10 },
+    autoClosingBrackets: 'always',
+    minimap: { enabled: false },
   };
 
   const handleEditorChange = (value: string | undefined) => {
@@ -21,21 +23,45 @@ const CodeEditor: React.FC = (): JSX.Element => {
     }
   };
 
+  const handleLanguageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setLanguage(event.target.value);
+  };
+
   const handleSubmit = () => {
     console.log(code);
   };
 
   return (
-    <div>
-      <Editor
-        height="100px"
-        language="javascript"
-        theme="vs-dark"
-        value={code}
-        options={editorOptions}
-        onChange={handleEditorChange}
-      />
-      <button onClick={handleSubmit}>submit</button>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <select
+          onChange={handleLanguageChange}
+          value={language}
+          className={styles.languageMenu}
+        >
+          <option value="javascript">JavaScript</option>
+          <option value="python">Python</option>
+          <option value="java">Java</option>
+          <option value="c">C</option>
+          <option value="cpp">C++</option>
+          <option value="csharp">C#</option>
+        </select>
+      </div>
+      <div className={styles.body}>
+        <Editor
+          height="300px"
+          language={language}
+          theme="vs-dark"
+          value={code}
+          options={editorOptions}
+          onChange={handleEditorChange}
+        />
+      </div>
+      <div className={styles.footer}>
+        <button onClick={handleSubmit}>submit</button>
+      </div>
     </div>
   );
 };
