13e39986d90742043eae0bac405ddf07bbba3257
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-22 20:06:44 +0900

    refactor : props 수정(객체 속성 열거 -> props)

--- src/components/select-field.tsx
+++ src/components/select-field.tsx
@@ -1,27 +1,25 @@
 import { UseFormRegisterReturn } from 'react-hook-form';
 import styles from 'styles/select-field.module.css';
 
+interface IProps {
+  label: string;
+  register: UseFormRegisterReturn;
+  options: Option[];
+}
+
 // select option 타입
 type Option = {
   value: string | number;
   name: string;
 };
 
-export default function SelectField({
-  label,
-  register,
-  options,
-}: {
-  label: string;
-  register: UseFormRegisterReturn;
-  options: Option[];
-}) {
+export default function SelectField(props: IProps) {
   return (
     <div className={styles['select-box']}>
       <div className={styles.select}>
-        <label htmlFor={register.name}>{label} </label>
-        <select id={register.name} {...register}>
-          {options.map((item: Option) => (
+        <label htmlFor={props.register.name}>{props.label} </label>
+        <select id={props.register.name} {...props.register}>
+          {props.options.map((item: Option) => (
             <option value={item.value} key={item.value}>
               {item.name}
             </option>
