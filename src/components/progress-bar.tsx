fd4b05b1a0425b81c2da0dbc6fa6b5cc6f3b182b
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-23 23:31:37 +0900

    feat : props 수정

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -1,13 +1,25 @@
 import ProgressBar from '@ramonak/react-progress-bar';
 
-export default function ProgressBarComponent() {
+interface IProps {
+  completed: number;
+  roundedValue: number;
+}
+
+export default function ProgressBarComponent(props: IProps) {
   return (
-    <ProgressBar
-      completed={75}
-      bgColor="#f11946"
-      height="20px"
-      borderRadius="10px"
-      isLabelVisible={false}
-    />
+    <div>
+      <div>
+        <ProgressBar
+          completed={props.completed}
+          bgColor="#f11946"
+          height="15px"
+          width="150px"
+          borderRadius="10px"
+          transitionDuration="0.3s"
+          customLabel={`${props.roundedValue}%`}
+          isLabelVisible={true}
+        />
+      </div>
+    </div>
   );
 }
