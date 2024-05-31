1aa8b799f60c1dccc42609db006d4828a78f6dab
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-31 22:11:12 +0900

    feat : 새로고침시 경고창 출력

--- src/components/tabs/Room-copy.tsx
+++ src/components/tabs/Room-copy.tsx
@@ -10,14 +10,42 @@ interface IProps {
   dockLayoutRef: React.RefObject<DockLayout>; // DockLayout 컴포넌트에 대한 RefObject 타입 지정
 }
 
+interface IProblem {
+  id: number;
+  title: string;
+  algorithmClassification: string;
+  problemLevel: string;
+  problemDescription: string;
+  inputDescription: string;
+  outputDescription: string;
+  hint: string;
+}
+
 const RoomCopy = (props: IProps) => {
   const [isGameStart, setIsGameStart] = useState<boolean>(false); // 게임 시작 여부
   const [chatIsHide, setChatIsHide] = useState<boolean>(false); // 채팅 표시 여부
   const [roomStatus, setRoomStatus] = useState(props.data.roomStatus); // 방 상태
   const [userStatus, setUserStatus] = useState(props.data.userStatus); // 유저들 상태
   const [message, setMessage] = useState(null);
+  const [problems, setProblems] = useState<IProblem[]>([]);
   const { webSocketClient, setRoomSubscription } = useWebSocketStore();
   const userId = localStorage.getItem('id');
+
+  // 새로고침을 막는 함수
+  const preventClose = (e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = ''; // 이 코드는 Chrome에서 새로고침 경고를 활성화하는 데 필요
+  };
+
+  useEffect(() => {
+    // 컴포넌트 마운트 시 beforeunload 이벤트 리스너 추가
+    window.addEventListener('beforeunload', preventClose);
+
+    // 컴포넌트 언마운트 시 리스너 제거
+    return () => {
+      window.removeEventListener('beforeunload', preventClose);
+    };
+  }, []);
 
   useEffect(() => {
     if (!webSocketClient) return;
@@ -39,10 +67,12 @@ const RoomCopy = (props: IProps) => {
       message={message}
       userId={userId}
       dockLayoutRef={props.dockLayoutRef}
+      problems={problems}
       roomStatus={roomStatus}
       userStatus={userStatus}
       chatIsHide={chatIsHide}
       setChatIsHide={setChatIsHide}
+      setProblems={setProblems}
     />
   ) : (
     <WaitingRoom
