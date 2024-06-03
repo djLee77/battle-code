e86fdff80f62f865f3189faba112f1aab8c5c6bb
ehdrjs4502 <ehdrjs4502@naver.com>
2024-06-03 17:23:41 +0900

    feat : eventEmttier 라이브러리로 웹소켓 메시지 핸들링

---
+++ src/components/room/WaitingRoom.tsx
@@ -0,0 +1,206 @@
+import { AxiosResponse } from 'axios';
+import Chat from 'components/room/chat/Chat';
+import ModifyRoomModal from 'components/room/waiting-room/ModifyRoomModal';
+import RoomSettings from 'components/room/waiting-room/RoomSettings';
+import UserList from 'components/room/waiting-room/UserList';
+import RoomCustomButton from 'components/ui/RoomCustomButton';
+import DockLayout from 'rc-dock';
+import { useCallback, useEffect, useState } from 'react';
+import useWebSocketStore from 'store/useWebSocketStore';
+import styles from 'styles/room.module.css';
+import { IUserStatus } from 'types/roomType';
+import api from 'utils/axios';
+import emitter from 'utils/eventEmitter';
+import { removeTab } from 'utils/tabs';
+
+interface IProps {
+  userId: string | null;
+  userStatus: IUserStatus[];
+  roomStatus: any;
+  dockLayoutRef: React.RefObject<DockLayout>;
+  setIsGameStart: (isGameStart: boolean) => void;
+  setRoomStatus: (roomStatus: any) => void;
+  setUserStatus: (
+    updateFunction: (prevUserStatuses: IUserStatus[]) => IUserStatus[]
+  ) => void;
+}
+
+const WaitingRoom = (props: IProps) => {
+  const [isAllUsersReady, setIsAllUsersReady] = useState<boolean>(false); // 모든 유저 준비 여부
+
+  const { publishMessage, roomSubscribe } = useWebSocketStore();
+
+  useEffect(() => {
+    // 호스트 유저를 제외한 모든 유저의 isReady 상태 확인
+    const allUsersExceptHost = props.userStatus.filter(
+      (user: IUserStatus) => user.userId !== props.roomStatus.hostId
+    );
+    // 방에 호스트 혼자면 코드 실행 중지
+    if (allUsersExceptHost.length === 0) {
+      return;
+    }
+    const allReady = allUsersExceptHost.every(
+      (user: IUserStatus) => user.isReady
+    );
+
+    // 모든 유저가 준비 상태인지를 판단하여 상태 업데이트
+    setIsAllUsersReady(allReady);
+  }, [props.userStatus]);
+
+  useEffect(() => {
+    const handleMessages = (msg: any) => {
+      console.log('대기방 : ', msg);
+      // 유저 상태 업데이트
+      if (msg.updateUserStatus) {
+        props.setUserStatus((prev: any) =>
+          prev.map((user: any) =>
+            user.userId === msg.updateUserStatus.userId
+              ? msg.updateUserStatus
+              : user
+          )
+        );
+      }
+
+      //유저 입장
+      if (msg.enterUserStatus) {
+        props.setUserStatus((prevUserStatus) => [
+          ...prevUserStatus,
+          msg.enterUserStatus,
+        ]);
+      }
+
+      // 유저 퇴장
+      if (msg.leaveUserStatus) {
+        const leaveUserStatus = msg.leaveUserStatus;
+        // 방장이 퇴장할시
+        if (leaveUserStatus.isHost && leaveUserStatus.userId !== props.userId) {
+          alert('방장이 나갔습니다 ㅠㅠ');
+          removeTab(props.dockLayoutRef, `${props.roomStatus.roomId}번방`);
+          return;
+        }
+        // 유저가 퇴장할시
+        props.setUserStatus((prevUserStatus: IUserStatus[]) =>
+          prevUserStatus.filter(
+            (user) => user.userId !== leaveUserStatus.userId
+          )
+        );
+      }
+
+      // 방 설정 변경
+      if (msg.roomStatus) {
+        props.setRoomStatus(msg.roomStatus);
+      }
+
+      // 게임 시작
+      if (msg.gameStartInfo) {
+        props.setIsGameStart(true);
+      }
+    };
+
+    emitter.on('message', handleMessages);
+
+    // Cleanup 함수를 사용하여 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      emitter.off('message', handleMessages);
+    };
+  }, []);
+
+  const handleRoomLeave = useCallback(async (): Promise<void> => {
+    try {
+      const response: AxiosResponse = await api.post(
+        `v1/room/leave/${props.roomStatus.roomId}`,
+        {}
+      );
+      console.log(response);
+      removeTab(props.dockLayoutRef, `${props.roomStatus.roomId}번방`);
+      roomSubscribe.subscription?.unsubscribe();
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('요청 실패:', error.message);
+      } else {
+        console.error('알 수 없는 에러:', error);
+      }
+    }
+  }, [props.roomStatus.roomId, props.dockLayoutRef, roomSubscribe]);
+
+  const handleReady = useCallback((): void => {
+    const updateUser = props.userStatus.find(
+      (user) => user.userId === props.userId
+    );
+    if (updateUser) {
+      updateUser.isReady = !updateUser.isReady;
+      publishMessage(
+        `/app/room/${props.roomStatus.roomId}/update/user-status`,
+        updateUser
+      );
+    }
+  }, [props.userStatus, props.userId, props.roomStatus.roomId]);
+
+  const handleGameStart = useCallback(async (): Promise<void> => {
+    try {
+      const response: AxiosResponse = await api.post(
+        `v1/game/${props.roomStatus.roomId}/start`,
+        {}
+      );
+      props.setIsGameStart(true);
+      console.log(response);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('요청 실패:', error.message);
+      } else {
+        console.error('알 수 없는 에러:', error);
+      }
+    }
+  }, [props.roomStatus.roomId, props.setIsGameStart]);
+
+  return (
+    <div>
+      <>
+        <h1 className={styles.title}>{props.roomStatus.title}</h1>
+        {props.roomStatus.hostId === props.userId && (
+          <ModifyRoomModal data={props.roomStatus} />
+        )}
+      </>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <div className={styles.leftBody}>
+            <span>게임이 시작되면 문제가 표시됩니다!</span>
+          </div>
+          <div className={styles.leftFooter}>
+            <RoomCustomButton onClick={handleRoomLeave}>
+              나가기
+            </RoomCustomButton>
+          </div>
+        </div>
+        <div className={styles.center}>
+          <div className={styles.centerBody}>
+            <div className={styles.flexGrow}>
+              <UserList
+                userStatus={props.userStatus}
+                roomId={props.roomStatus.roomId}
+              />
+              <RoomSettings roomStatus={props.roomStatus} />
+            </div>
+          </div>
+          <div className={styles.centerFooter}>
+            {props.roomStatus.hostId === props.userId ? (
+              <RoomCustomButton
+                // disabled={!isAllUsersReady}
+                onClick={handleGameStart}
+              >
+                게임시작
+              </RoomCustomButton>
+            ) : (
+              <RoomCustomButton onClick={handleReady}>
+                준비완료
+              </RoomCustomButton>
+            )}
+          </div>
+        </div>
+        <Chat />
+      </div>
+    </div>
+  );
+};
+
+export default WaitingRoom;
