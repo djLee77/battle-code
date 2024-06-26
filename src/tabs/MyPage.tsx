adc68335ef6ed26d779a7614dd7c32d551db765d
ehdrjs4502 <ehdrjs4502@naver.com>
2024-06-26 18:30:30 +0900

    feat : 유저 전적 검색 구현

--- src/tabs/MyPage.tsx
+++ src/tabs/MyPage.tsx
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
 import RecordCard from 'components/user/RecordCard';
 import { useInfiniteQuery, useQuery } from 'react-query';
 import api from 'utils/axios';
 import styles from 'styles/my-page/user.module.css';
 import InfiniteScroll from 'react-infinite-scroller';
 import CircleProgress from 'components/user/CircleProgress';
+import InputField from 'components/ui/InputField';
+import { useForm } from 'react-hook-form';
 
 interface IMatchUsers {
   user: string;
@@ -20,107 +23,157 @@ interface IMatchHistory {
   usersResult: IMatchUsers[];
 }
 
-// 1페이지에 담을 데이터  양
+// 데이터 크기 설정
 const SIZE = 10;
 
 const fetchMatchHistory = async (pageParam: number, userId: string | null) => {
-  const response = await api.get(
-    `v1/recodes/${userId}?pageNo=${pageParam}&size=${SIZE}`
-  );
-
-  return response.data.data;
+  try {
+    const response = await api.get(
+      `v1/recodes/${userId}?pageNo=${pageParam}&size=${SIZE}`
+    );
+    return response.data.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(
+        `Error ${error.response.status}: ${error.response.data.message}`
+      );
+    } else {
+      throw new Error(error.message);
+    }
+  }
 };
 
 const fetchOverAlls = async (userId: string | null) => {
-  const response = await api.get(`v1/recodes/${userId}/overalls`);
+  try {
+    const response = await api.get(`v1/recodes/${userId}/overalls`);
+    return response.data.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(
+        `Error ${error.response.status}: ${error.response.data.message}`
+      );
+    } else {
+      throw new Error(error.message);
+    }
+  }
+};
 
-  return response.data.data;
+type FormValue = {
+  userId: string;
 };
 
 const MyPage = () => {
-  const userId = localStorage.getItem('id');
+  const [searchedUserId, setSearchedUserId] = useState<string | null>(
+    localStorage.getItem('id')
+  );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValue>();
+
   const {
     data: matchHistory,
     fetchNextPage,
     hasNextPage,
     isLoading: matchHistoryIsLoading,
+    isError: matchHistoryIsError,
   } = useInfiniteQuery<any>(
-    ['matchHistory', userId], // queryKey에 userId 포함
-    ({ pageParam = 0 }) => fetchMatchHistory(pageParam, userId),
+    ['matchHistory', searchedUserId],
+    ({ pageParam = 0 }) => fetchMatchHistory(pageParam, searchedUserId),
     {
       getNextPageParam: (lastPage) => {
         return lastPage.currentPage !== lastPage.totalPage
           ? lastPage.currentPage + 1
           : undefined;
       },
-      enabled: !!userId, // userId가 있을 때만 쿼리 실행
+      enabled: !!searchedUserId, // userId가 있을 때만 쿼리 실행
       cacheTime: 600000, // 10분
       staleTime: 60000, // 1분
     }
   );
 
-  const { data: overAlls, isLoading } = useQuery(
-    ['overAlls', userId],
-    () => fetchOverAlls(userId),
+  const {
+    data: overAlls,
+    isLoading: overAllsIsLoading,
+    isError: overAllsIsError,
+  } = useQuery(
+    ['overAlls', searchedUserId],
+    () => fetchOverAlls(searchedUserId),
     {
-      enabled: !!userId, // userId가 있을 때만 쿼리 실행
+      enabled: !!searchedUserId, // userId가 있을 때만 쿼리 실행
       cacheTime: 600000, // 10분
       staleTime: 60000, // 1분
     }
   );
 
-  if (matchHistoryIsLoading) return <div className="loading">Loading...</div>;
-  if (isLoading) return <div className="loading">Loading...</div>;
-
-  console.log(matchHistory);
+  const onSubmit = (data: FormValue) => {
+    setSearchedUserId(data.userId);
+  };
 
   return (
     <div className={styles[`user-container`]}>
-      <div className={styles[`user-info`]}>
-        <h2>{userId}</h2>
-      </div>
-      <div className={styles[`record-container`]}>
-        <div className={styles[`circle-box`]}>
-          <h4>
-            {overAlls.total}전 {overAlls.win}승 {overAlls.draw}무{' '}
-            {overAlls.lose}패
-          </h4>
-          <CircleProgress
-            win={overAlls.win}
-            draw={overAlls.draw}
-            lose={overAlls.lose}
-            total={overAlls.total}
-            size={300}
-            strokeWidth={40}
-            winColor="#3278FF"
-            drawColor="#555555"
-            loseColor="#FF5F58"
+      <div style={{ display: 'flex' }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <InputField
+            placeholder="유저 검색"
+            type="text"
+            register={register('userId', { required: true })}
           />
-        </div>
-        <div
-          className={styles[`record-box`]}
-          style={{ height: '80vh', overflowY: 'auto' }}
-        >
-          <InfiniteScroll
-            loadMore={() => fetchNextPage()}
-            hasMore={hasNextPage}
-            loader={
-              <div className={styles.loader}>
-                <div className={styles.spinner}></div>
-              </div>
-            }
-            useWindow={false}
-          >
-            {matchHistory?.pages.map((page: any, index: any) => (
-              <div key={index}>
-                {page.matchRecodeList.map((record: IMatchHistory) => (
-                  <RecordCard key={record.matchId} record={record} />
-                ))}
-              </div>
-            ))}
-          </InfiniteScroll>
-        </div>
+        </form>
       </div>
+      {matchHistoryIsLoading && <div>Loading...</div>}
+      {matchHistoryIsError && <div>존재하지 않는 사용자입니다.</div>}
+      {!matchHistoryIsLoading && !matchHistoryIsError && (
+        <>
+          <div className={styles[`user-info`]}>
+            <h2>{searchedUserId}</h2>
+            <div className={styles.search}></div>
+          </div>
+          <div className={styles[`record-container`]}>
+            <div className={styles[`circle-box`]}>
+              <h4>
+                {overAlls?.total}전 {overAlls?.win}승 {overAlls?.draw}무{' '}
+                {overAlls?.lose}패
+              </h4>
+              <CircleProgress
+                win={overAlls?.win}
+                draw={overAlls?.draw}
+                lose={overAlls?.lose}
+                total={overAlls?.total}
+                size={300}
+                strokeWidth={40}
+                winColor="#3278FF"
+                drawColor="#555555"
+                loseColor="#FF5F58"
+              />
+            </div>
+            <div
+              className={styles[`record-box`]}
+              style={{ height: '80vh', overflowY: 'auto' }}
+            >
+              <InfiniteScroll
+                loadMore={() => fetchNextPage()}
+                hasMore={hasNextPage}
+                loader={
+                  <div className={styles.loader}>
+                    <div className={styles.spinner}></div>
+                  </div>
+                }
+                useWindow={false}
+              >
+                {matchHistory?.pages.map((page: any, index: any) => (
+                  <div key={index}>
+                    {page.matchRecodeList.map((record: IMatchHistory) => (
+                      <RecordCard key={record.matchId} record={record} />
+                    ))}
+                  </div>
+                ))}
+              </InfiniteScroll>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
