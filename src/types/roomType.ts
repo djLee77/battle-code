c338f85e92842c90c05451a27da2c3080934d30d
ehdrjs4502 <ehdrjs4502@naver.com>
2024-07-03 21:12:29 +0900

    feat : 대기방 타입 설정

--- src/types/roomType.ts
+++ src/types/roomType.ts
@@ -15,8 +15,22 @@ export interface IRoomList {
 }
 
 // 대기방 타입
+export interface IRoom {
+  roomId: number;
+  hostId: string;
+  title: string;
+  isLocked: boolean;
+  isStarted: boolean;
+  problemLevel: number;
+  limitTime: number;
+  maxSubmitCount: number;
+  maxUserCount: number;
+  language: string;
+}
+
+// 대기방 타입
 export interface IRoomStatus {
-  roomStatus: IRoomList;
+  roomStatus: IRoom;
   userStatus: IUserStatus[]; // UserStatus 배열 타입
 }
 
